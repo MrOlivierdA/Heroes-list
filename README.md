5dcd09bb6b26e1569b098528fa64b53f7e8bce6b
MrOlivierdA <olivier.dardenne.pro@gmail.com>
2022-06-29 09:41:55 +0200

    projet1

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# Heroes-list
+# Character-manager-js
+
+## About this project:
+
+| Challenge Parameters | Challenge Details           |
+| :------------------- | :-------------------------- |
+| Repository           | `character-manager-js`      |
+| Challenge type       | `consolidation challenge`   |
+| Duration             | `5 days`                    |
+| Deployment method    | `GitHub pages` or `Netlify` |
+| Group composition    | `Duo`                       |
+
+## Objectives
+
+- Use best JS practices
+- Read documentation and test a REST API
+- Use a REST API with HTTP requests
+- Create a typical asynchronous flow : send asynchronous (promises or async/await) requests to a remote server and process the results
+- DOM manipulation: changing the DOM based on results of HTTP requests
+
+## The job
+
+A Comics fan client would like to manage his favorites characters on a dashboard. He contacts you to create an app that will be able to view, edit, create all his favorites characters.
+
+In this project, you will use the [**Character Database API**](https://character-database.becode.xyz/) to make/fetch a Character Manager. This project ask you to perform HTTP Request.
+
+The root endpoint of the API is the following : https://character-database.becode.xyz/
+
+This is a frontend project, you have to care about the appearance of your application and create a custom template.
+
+### The features
+
+#### Characters list page
+
+This page will be the first one displayed when you open the app. Display the list of all the characters contained inside the database. You can use a table or a list of cards to display them, at your convenience.
+
+Make sure the following features will be present for each character:
+
+- Name
+- A short description. Be aware that the `description` field is in Markdown and, of course, we want to display it as HTML in your application.
+- An image. **Hint**: [Data URIs](https://css-tricks.com/data-uris/)
+- A button to create a new character
+- A way to open the corresponding Character
+
+#### Single character
+
+This page should display:
+
+- The name and image for the character.
+- A **full description** of the character. Be aware that the `description` field is in Markdown and, of course, we want to display it as HTML in your application.
+- A way to open the Character editor
+- A way to delete the character (You **must** add some kind of confirmation, like "Are you sure you want to delete this character ?")
+
+#### Character creation
+
+When we click on the character creation button in the Characters list page, we should get redirect to this page. This page should contain a form with an input for each of the following fields:
+
+- `name`
+- `shortDescription`
+- `image` **Hint**: [Reading local files in JavaScript](https://www.html5rocks.com/en/tutorials/file/dndfiles/)
+- `description` (You can just let the user enter Markdown but a Wysiwyg editor generating Markdown would be much cooler)
+
+When the form is completed create the character in the API and redirect to the character list page.
+
+#### Character editor
+
+This part should be similar to the Character creation page except it allows to edit an existing character.  
+It should also contain a button to delete the character. When this deleting an item, display a modal asking for confirmation.
+
+#### Search input (optionnal)
+
+The client can search a character by name or by ID.
+
+## Constraints
+
+- Your website must be deployed somewhere. We recommend [Github Pages](https://pages.github.com) or [Netlify](https://www.netlify.com), two great (and free) solutions.
+- You have to use the best practises in JS, as much as possible 👉 [Read these instructions](jsbestpractises.md)
+- You must use Sass.
+
+---
+
+## Tips
+
+### Bundler
+
+To help you to compile your SCSS files and your modern JavaScript files, we advise you to use a bundler.
+
+Two of the most easier to deal with are **Parcel.js** & **ViteJS**, have a look at the documentation of [Parcel](https://parceljs.org/docs/) or [ViteJS](https://vitejs.dev/guide/)
+
+### .gitignore
+
+If you use `npm`, don't forget to add `node_modules` in your `.gitignore` file before beginning.
+
+### API REST
+
+Explanations about what can do a [REST API](https://github.com/leny/klopedi/blob/master/rest/rest.md)
+
+### CSS frameworks
+
+You can use a _css frameworks_ like [Bootstrap](https://getbootstrap.com/) or [Materialize](https://materializecss.com/).
+It will help you to design quickly your application.
+
+Even more, you can search and use some free pre-made templates like [this](https://github.com/startbootstrap/startbootstrap-heroic-features).
+
+### HTTP requests
+
+To help you to perfom HTTP Requests, use the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API).
+
+An another nice solution is a tool called [axios](https://github.com/axios/axios). Axios is a promise based HTTP client for the browser and Node.js.
+You can install it with npm :
+
+```bash
+npm install --save axios
+```
+
+Then to import it in your JS files:
+
+```javascript
+const axios = require("axios");
+```
+
+Using axios it is strongly recommended to use the [async/await](https://javascript.info/async-await) syntax when working with asynchronous operations.
+
+### Postman
+
+To test APIs rapidly before coding you can use [Postman](https://www.getpostman.com/).
+To start using it today with the Character Database API you can **import** [this Postman collection](https://static.becode.xyz/character-database/characters-database.postman_collection.json).
+
+You can follow this serie of video to know how to manage it : [Postman Tutorial](https://www.youtube.com/watch?v=juldrxDrSH0&list=PLhW3qG5bs-L-oT0GenwPLcJAPD_SiFK3C&ab_channel=AutomationStepbyStep-RaghavPal)
+
+## Good luck for this project and show us your favorite serie/movie/game characters!
+
+![](https://media.giphy.com/media/tliXLSkzfq2C4/giphy.gif)
---
+++ assets/script.js
@@ -0,0 +1,191 @@
+const characterId = new Array();
+let image = "";
+const cardName = document.getElementsByClassName('name-for-modal');
+const shortDescription = document.getElementsByClassName('short-for-modal');
+const longDescription = document.getElementsByClassName('long-for-modal');
+const cardImage = document.getElementsByClassName('image-for-modal');
+
+// clone character's cards
+async function displayCharactersCards() {
+  try {
+    const response = await fetch('https://character-database.becode.xyz/characters');
+    const character = await response.json();
+
+    const cardTemplate = document.querySelector('#template');
+    const target = document.querySelector('#target');
+
+    character.forEach(({ name, shortDescription, image, description, id }) => {
+      const cardClone = cardTemplate.cloneNode(true).content;
+
+      cardClone.querySelector('#name').innerHTML = name;
+      cardClone.querySelector('#short-description').innerHTML = shortDescription;
+      cardClone.querySelector('#image').src = `data:image/*;base64,${image}`;
+      cardClone.querySelector('#long-description').innerHTML = description;
+
+      target.appendChild(cardClone);
+
+      characterId.push(id);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+let callAllFunctions = displayCharactersCards();
+
+callAllFunctions.then(() => {
+  openCharacterCard();
+  createImage();
+  createCharacter();
+  editCharacter();
+  deleteCharacter();
+})
+
+// open character card
+function openCharacterCard() {
+  const longDescriptionButton = document.getElementsByClassName('long-description-button');
+
+  for (let i = 0; i < longDescriptionButton.length; i++) {
+    longDescriptionButton[i].addEventListener('click', function () {
+
+      let modalName = document.getElementById('name-modal');
+      let modalShortDescription = document.getElementById('short-modal-description');
+      let modalLongDescription = document.getElementById('long-modal-description');
+      let modalImage = document.getElementById('modal-image');
+
+      modalName.textContent = cardName[i].textContent;
+      modalShortDescription.textContent = shortDescription[i].textContent;
+      modalLongDescription.textContent = longDescription[i].textContent;
+      modalImage.src = cardImage[i].src;
+    });
+  }
+}
+
+//create a character
+async function createCharacter() {
+  document.getElementById('create').addEventListener('click', async () => {
+    try {
+      document.getElementById('submit').addEventListener('click', async () => {
+        const inputs = Array.from(document.getElementsByClassName("inputs"));
+        const values = inputs.map(({ value }) => value.trim());
+
+        if (values.some((value) => value === "")) {
+          alert("there's an empty input!");
+          return;
+        }
+        else {
+          const [name, shortDescription, description] = values;
+          const response = await fetch('https://character-database.becode.xyz/characters', {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name,
+              shortDescription,
+              description,
+              image,
+            }),
+          });
+
+          const createdCharacter = await response.json();
+          console.log(createdCharacter);
+          location.reload();
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+//create image
+async function createImage() {
+  document.querySelector("#input-image").addEventListener("change", (element) => {
+    const file = element.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      image = reader.result.replace('data:', '').replace(/^.+,/, '');
+    };
+    reader.readAsDataURL(file)
+  });
+}
+
+//edit character
+async function editCharacter() {
+  const outerEditButton = document.getElementsByClassName('outer-edit');
+
+  for (let i = 0; i < outerEditButton.length; i++) {
+    outerEditButton[i].addEventListener('click', async () => {
+      const id = characterId[i];
+      try {
+        const response = await fetch(`https://character-database.becode.xyz/characters/${id}`);
+        const character = await response.json();
+
+        document.getElementById('input-name').value = character.name;
+        document.getElementById('input-short-description').value = character.shortDescription;
+        document.getElementById('input-long-description').textContent = character.description;
+        image = character.image;
+
+        document.getElementById('submit').addEventListener('click', async () => {
+          const inputs = Array.from(document.getElementsByClassName("inputs"));
+          const values = inputs.map(({ value }) => value.trim());
+          const [name, shortDescription, description] = values;
+
+          if (values.some((value) => value === "")) {
+            alert("there's an empty input!");
+            return;
+          }
+          else {
+            const edit = await fetch(`https://character-database.becode.xyz/characters/${id}`, {
+              method: 'PUT',
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name,
+                shortDescription,
+                description,
+                image
+              }),
+            });
+            const editedCharacter = await edit.json();
+            console.log(editedCharacter);
+            location.reload();
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+}
+
+// delete a character
+function deleteCharacter() {
+  const deleteButton = document.getElementsByClassName('delete');
+
+  for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', async function () {
+
+      if (confirm('Are you sure you want to delete this character?')) {
+        const id = characterId[i];
+
+        try {
+          const response = await fetch(`https://character-database.becode.xyz/characters/${id}`, {
+            method: 'DELETE'
+          });
+
+          const deletedCharacter = await response.json();
+          console.log(deletedCharacter);
+
+          location.reload();
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        alert('This character has not been deleted');
+      }
+    });
+  };
+}
